3290ee8e9228ce482ed62f46d3314f65f35ebeea
chengjing1181122598 <1181122598@qq.com>
2016-12-17 22:36:24 +0800

    123

--- js/common/common.js
+++ js/common/common.js
@@ -1,5 +1,5 @@
-﻿var data_apps = "http://localhost:8080/vicky_data";
-var file_apps = "http://localhost:8084/vicky_file";
+﻿var data_apps = "http://localhost:8083/vicky_data";
+var file_apps = "http://localhost:8083/vicky_file";
 var html_apps = "/vicky_html";
 var successStatus = "success";
 var errorStatus = "error";
